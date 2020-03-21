76b1f83f51a68a9775928cdbfa400c96ad28f354
AeDani <31882578+AeDani@users.noreply.github.com>
2020-03-21 21:00:11 +0100

    Updates

---
+++ precache-manifest.d6db09c389509ea37083ae7a68abfc8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751338c1a5b395856a7696e7f16cc0b9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8087921f274c02b2cea0",
+    "url": "/monsters-rolodex/static/css/main.e0d4c728.chunk.css"
+  },
+  {
+    "revision": "a683d3c15696d61584b7",
+    "url": "/monsters-rolodex/static/js/2.a234ae2c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.a234ae2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8087921f274c02b2cea0",
+    "url": "/monsters-rolodex/static/js/main.6cd04c4e.chunk.js"
+  },
+  {
+    "revision": "ed75eac08a3bf1a4b090",
+    "url": "/monsters-rolodex/static/js/runtime-main.1d84e0ed.js"
+  }
+]);
